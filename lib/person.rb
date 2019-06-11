aec0983e9e7f1e6a4f89d09e6ce870bb4a43b1f9
JangmiTokki <13933554+JangmiTokki@users.noreply.github.com>
2019-06-10 23:04:47 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+   
+    attr_reader :name       
+    def initialize(name)
+        @name = name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+
+end
